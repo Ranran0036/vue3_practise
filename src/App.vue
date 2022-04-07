4c849b136582721adba165137abc4a75ffbf531c
Ranran <ranran0036@163.com>
2022-04-07 17:26:51 +0800

    practice : vue3 ref

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>一个人的信息</h1>
+	<h2>姓名：{{name}}</h2>
+	<h2>年龄： {{age}}</h2>
+	<button @click="changeInfo">Change Info</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from '@vue/reactivity';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+	setup () {
+		let name = ref('Tom Cruise');
+		let age = ref(56);
+
+		function changeInfo (){
+			name.value = 'Tom Hanks';
+			age.value = 68
+		}
+
+		return {
+			name,
+			age,
+			changeInfo
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
