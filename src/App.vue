ca7c6a18e119c3dc8ea9943b0a5a81a0181ee037
Ranran <ranran0036@163.com>
2022-04-07 22:02:57 +0800

    practice : vue3 ref_2

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,45 @@
 	<h1>一个人的信息</h1>
 	<h2>姓名：{{name}}</h2>
 	<h2>年龄： {{age}}</h2>
+	<h3>职业：{{job.work}}</h3>
+	<h3>薪水：{{job.salary}}</h3>
 	<button @click="changeInfo">Change Info</button>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity';
-export default {
-    name: 'App',
-	setup () {
-		let name = ref('Tom Cruise');
-		let age = ref(56);
+	import { ref } from '@vue/reactivity';
+	export default {
+		name: 'App',
+		setup () {
+			let name = ref('Tom Cruise');
+			let age = ref(56);
+			let job = ref({
+				work : 'actor',
+				salary : '30k'
+			});
 
-		function changeInfo (){
-			name.value = 'Tom Hanks';
-			age.value = 68
-		}
+			function changeInfo (){
+				job.value.work = 'artist';
+				job.value.salary = '40k';
+			}
 
-		return {
-			name,
-			age,
-			changeInfo
+			return {
+				name,
+				age,
+				changeInfo,
+				job
+			}
 		}
 	}
-}
 </script>
 
 <style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
