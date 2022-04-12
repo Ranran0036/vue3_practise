38a9f83f61886d3a0bef5cb4ed240d0a56782122
Ranran <ranran0036@163.com>
2022-04-12 20:21:16 +0800

    learn : vue3_toRef&toRefs

--- src/components/MyDemo.vue
+++ src/components/MyDemo.vue
@@ -1,26 +1,42 @@
 <template>
-    <h2>当前求和为： {{ sum }}</h2>
-    <button @click="sum++">点我加1</button>
-    <hr />
-    <h2>当前鼠标点击的坐标为：x-{{ point.x }} , y-{{ point.y }}</h2>
+    <h2>
+        姓名：{{ person.name }} &nbsp; 年龄：{{ person.age }} &nbsp; 薪资：{{
+            person.job.job1.salary
+        }}K
+    </h2>
+    <button @click="age++">点我增加年龄</button>
+    <button @click="name += '~'">点我修改姓名</button>
+    <button @click="salary++">点我增加薪资</button>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
-import usePoint from "../hooks/usePoint";
+import { reactive, toRef, toRefs } from "@vue/reactivity";
 export default {
     //定义组件名
     name: "MyDemo",
 
     // 初始化数据
     setup() {
-        let sum = ref(1);
+        let person = reactive({
+            name: "Tom Cruise",
+            age: 56,
+            job: {
+                job1: {
+                    salary: 10,
+                },
+            },
+        });
 
-        let point = usePoint();
+        //解构赋值
+        let { name, age } = toRefs(person);
+        //toRefs 只会为源对象中包含的 property 生成 ref。如果要为特定的 property 创建 ref，则应当使用 toRef
+        let salary = toRef(person.job.job1, "salary");
 
         return {
-            sum,
-            point,
+            person,
+            name,
+            age,
+            salary,
         };
     },
 };
---
+++ src_hook/components/MyDemo.vue
@@ -0,0 +1,27 @@
+<template>
+    <h2>当前求和为： {{ sum }}</h2>
+    <button @click="sum++">点我加1</button>
+    <hr />
+    <h2>当前鼠标点击的坐标为：x-{{ point.x }} , y-{{ point.y }}</h2>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import usePoint from "../hooks/usePoint";
+export default {
+    //定义组件名
+    name: "MyDemo",
+
+    // 初始化数据
+    setup() {
+        let sum = ref(1);
+
+        let point = usePoint();
+
+        return {
+            sum,
+            point,
+        };
+    },
+};
+</script>
